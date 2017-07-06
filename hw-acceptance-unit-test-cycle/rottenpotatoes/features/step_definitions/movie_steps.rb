a5aa0e3f821c11fd083074a7660213f1441c7fcc
Aayush sharma <greatash90@gmail.com>
2017-07-06 23:28:47 +0530

    hw acceptance-unit

---
+++ hw-acceptance-unit-test-cycle/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,11 @@
+#Step defination for director features
+
+Given /the following movies exist/ do |movie_table|
+  movie_table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
+
+Then /the director of "(.*)" should be "(.*)"/ do |movie_name, director_name|
+  page.should have_content(director_name)
+end
